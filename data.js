1a496e4a9d148091cc1af60f7efe1b822a087673
sevvalahiskali <sevvalahiskali96@gmail.com>
2025-04-12 23:34:32 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-interior-panorama",
+      "name": "Interior Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.819638287361399,
+          "pitch": -0.1471110806276439,
+          "title": "<span style=\"font-size: 13px;\">Staircase above the kitchen counter</span>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.7801634570430913,
+          "pitch": 0.647292401380291,
+          "title": "<span style=\"font-size: 13px;\">Fireplace for rear living room</span>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.5562317385428166,
+          "pitch": 0.09260746839166778,
+          "title": "<span style=\"font-size: 13px;\">Fireplace for entrance living room</span>",
+          "text": "<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Bramwell Close",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
